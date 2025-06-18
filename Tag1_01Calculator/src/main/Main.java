69f1759d62a25dbf90e98e456220f5494439988d
Jo Wagner <jowagner@limago.de>
2025-06-18 12:06:16 +0200

    initial commit

--- Tag1_01Calculator/src/main/Main.java
+++ Tag1_01Calculator/src/main/Main.java
@@ -1,18 +1,27 @@
 package main;
 
 import client.CalcClient;
+import common.LoggerProxy;
 import math.Calculator;
 import math.CalculatorImpl;
 import math.CalculatorLogger;
 import math.CalculatorSecure;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        Instant start = Instant.now();
+        //....
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+        System.out.println("Duration: " + duration.toMillis());
 
         Calculator calculator = new CalculatorImpl();
-        calculator = new CalculatorLogger(calculator);
+        //calculator = new CalculatorLogger(calculator);
+        calculator = (Calculator) LoggerProxy.newInstance(calculator);
         calculator = new CalculatorSecure(calculator);
         CalcClient calcClient = new CalcClient(calculator);
         calcClient.go();
---
+++ Webapp/src/main/java/de/fi/webapp/aspects/LoggerAspect.java
@@ -0,0 +1,47 @@
+package de.fi.webapp.aspects;
+
+import de.fi.webapp.services.PersoneServiceException;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggerAspect {
+
+    @Before(value="execution(public * de.fi.webapp.presentation.v1.PersonenQueryController.*(..))")
+    public void logAdvice(final JoinPoint joinPoint) throws PersoneServiceException {
+        log.warn(String.format(
+                "##################### Methode  %s wurde aufgerufen ########################"
+                , joinPoint.getSignature().getName()));
+    }
+
+    @AfterReturning(value="execution(public * de.fi.webapp.presentation.v1.PersonenQueryController.*(..))", returning = "result")
+    public void logAdviceAfterReturning(final JoinPoint joinPoint, Object result) throws PersoneServiceException {
+
+        log.warn(String.format("############################# Afterreturning: %s ######################", joinPoint.getSignature().getName()));
+        log.warn(String.format("############################# Result: %s ######################", result.toString()));
+    }
+
+    @After(value="execution(public * de.fi.webapp.presentation.v1.PersonenQueryController.*(..))")
+    public void logAdviceAfter(final JoinPoint joinPoint) throws PersoneServiceException {
+
+        log.warn(String.format("############################# After %s ######################", joinPoint.getSignature().getName()));
+
+    }
+
+    @AfterThrowing(value="execution(public * de.fi.webapp.presentation.v1.PersonenQueryController.*(..))", throwing = "ex")
+    public void afterThrowing(final JoinPoint joinPoint, Throwable ex) {
+
+        log.warn(String.format("############################# Afterreturning: %s ######################", joinPoint.getSignature().getName()));
+        log.warn(String.format("############################# Exception: %s ######################", ex.toString()));
+    }
+
+    @Around(value="execution(public * de.fi.webapp.presentation.v1.PersonenQueryController.*(..))")
+    public Object around(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return joinPoint.proceed();
+    }
+}
